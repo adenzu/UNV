f227e634ad51c38fec9b34925fa55614a679483d
Eren <57950444+adenzu@users.noreply.github.com>
2024-01-01 21:43:45 +0300

    added angular speed physics helper function

--- Assets/Scripts/PhysicsHelper.cs
+++ Assets/Scripts/PhysicsHelper.cs
@@ -12,7 +12,9 @@ public static class PhysicsHelper
     public static void ApplyForceToReachVelocity(Rigidbody rigidbody, Vector3 velocity, float force = 1, ForceMode mode = ForceMode.Force)
     {
         if (force == 0 || velocity.magnitude == 0)
+        {
             return;
+        }
 
         velocity += velocity.normalized * 0.2f * rigidbody.drag;
 
@@ -26,8 +28,32 @@ public static class PhysicsHelper
         }
         else
         {
-            var velocityProjectedToTarget = (velocity.normalized * Vector3.Dot(velocity, rigidbody.velocity) / velocity.magnitude);
+            var velocityProjectedToTarget = velocity.normalized * Vector3.Dot(velocity, rigidbody.velocity) / velocity.magnitude;
             rigidbody.AddForce((velocity - velocityProjectedToTarget) * force, mode);
+        }
+    }
+
+    public static void ApplyAccelerationToReachAngularSpeed(Rigidbody rigidbody, float angularSpeed, float acceleration = 1, ForceMode mode = ForceMode.Acceleration)
+    {
+        if (acceleration == 0 || angularSpeed == 0)
+        {
+            return;
+        }
+
+        angularSpeed += Mathf.Sign(angularSpeed) * 0.2f * rigidbody.angularDrag;
+
+        //acceleration = 1 => need 1 s to reach angularVelocity (if mass is 1) => acceleration can be max 1 / Time.fixedDeltaTime
+        acceleration = Mathf.Clamp(acceleration, -1 / Time.fixedDeltaTime, 1 / Time.fixedDeltaTime);
+
+        //dot product is a projection from rhs to lhs with a length of result / lhs.magnitude https://www.youtube.com/watch?v=h0NJK4mEIJU
+        if (rigidbody.angularVelocity.magnitude == 0)
+        {
+            rigidbody.AddTorque(0, acceleration, 0, mode);
+        }
+        else
+        {
+            float angularSpeedDifference = angularSpeed - rigidbody.angularVelocity.y;
+            rigidbody.AddTorque(0, angularSpeedDifference * acceleration, 0, mode);
         }
     }
 }
